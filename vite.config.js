bb6b377d78c542843dd560adecf32d9306d55df7
joseph <josephchome888@gmail.com>
2025-04-06 18:47:43 +0900

    [init] 빌드 파일 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: '../url-shortener-back/src/main/resources/static',
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080',
     },
   },
 })

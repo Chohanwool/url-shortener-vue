7ad3abcc5bde24e225d790ca48eb269074c470bd
joseph <josephchome888@gmail.com>
2025-04-06 19:26:19 +0900

    [init] axios 패키지 설치 및 유저 데이터 API 요청 테스트트

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <h1>사용자 목록</h1>
+      <ul v-if="users.length">
+        <li v-for="user in users" :key="user.email">{{ user.name }} ({{ user.email }})</li>
+      </ul>
+      <p v-else>불러오는 중 또는 사용자가 없습니다.</p>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+const users = ref([])
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('/api/users')
+    users.value = response.data
+  } catch (error) {
+    console.error('❌ 사용자 정보 불러오기 실패:', error)
+  }
+})
+</script>
 
 <style>
 @media (min-width: 1024px) {
@@ -10,6 +32,8 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    justify-content: center;
+    flex-direction: column;
   }
 }
 </style>
